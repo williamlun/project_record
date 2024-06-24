8d9e341bb79f0ef8a40321b11da383b958c851b3
William Leung <leungwilliamlun@gmail.com>
2024-06-23 22:08:13 -0400

    Record router draft.

--- python_backend/src/routers/record.py
+++ python_backend/src/routers/record.py
@@ -13,6 +13,11 @@ router = fastapi.APIRouter()
 # NOTE: Record related operations
 # CRUD
 # Create a new record
+# Read a record
+# Read a table of records
+# query record
+# update record
+# delete record
 
 
 @router.post("/record", response_model=schema.table.Record)
@@ -22,8 +27,37 @@ def create_record(record: schema.table.Record):
     return record
 
 
-# Read single record
-# Read a table of records
-# query record
-# update record
-# delete record
+@router.get("/record/{table_id}", response_model=list[schema.table.Record])
+def get_table(table_id: str):
+    """Get a table of records"""
+    logger.info(f"Get table: {table_id}")
+    pass
+
+
+@router.get("/record/{table_id}/query", response_model=list[schema.table.Record])
+def query_record(table_id: str):
+    """Query records"""
+    logger.info(f"Query records")
+    pass
+
+
+@router.get("/record/{table_id}/{record_id}", response_model=schema.table.Record)
+def get_record(table_id: str, record_id: str):
+    """Get a record"""
+    logger.info(f"Get record: {record_id}")
+    pass
+
+
+@router.put("/record/{table_id}/{record_id}", response_model=schema.table.Record)
+def update_record(table_id: str, record_id: str, record: schema.table.Record):
+    """Update a record"""
+    logger.info(f"Update record: {record_id}")
+    return record
+    pass
+
+
+@router.delete("/record/{table_id}/{record_id}")
+def delete_record(table_id: str, record_id: str):
+    """Delete a record"""
+    logger.info(f"Delete record: {record_id}")
+    pass
