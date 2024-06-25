160c86eff4436c58a75e79fb51599bd40e5c7805
William Leung <leungwilliamlun@gmail.com>
2024-06-24 22:47:10 -0400

    Update record router

--- python_backend/src/routers/record.py
+++ python_backend/src/routers/record.py
@@ -1,11 +1,12 @@
 """Router for record related operations"""
 
-import datetime
+import pydantic
 
 import fastapi
 from loguru import logger
 
 import schema
+import service
 
 router = fastapi.APIRouter()
 
@@ -20,44 +21,75 @@ router = fastapi.APIRouter()
 # delete record
 
 
-@router.post("/record", response_model=schema.table.Record)
+@router.post("/record/{table_id}", response_model=schema.table.Record)
 def create_record(record: schema.table.Record):
     """Create a new record"""
     logger.info(f"Create new record: {record}")
-    return record
+    response = service.record.RecordService(
+        "record_project_record_table"
+    ).create_record(record)
+    return response
 
 
-@router.get("/record/{table_id}", response_model=list[schema.table.Record])
+@router.get("table/{table_id}/record", response_model=list[schema.table.Record])
 def get_table(table_id: str):
     """Get a table of records"""
     logger.info(f"Get table: {table_id}")
-    pass
+    response = service.record.RecordService("record_project_record_table").query_record(
+        table_id
+    )
+    return response
 
 
-@router.get("/record/{table_id}/query", response_model=list[schema.table.Record])
-def query_record(table_id: str):
+@router.get("table/{table_id}/record/query", response_model=list[schema.table.Record])
+def query_record(
+    table_id: str,
+    limit: int = 10,
+    category: schema.common.RecordCategory = schema.common.RecordCategory.RECORD,
+    created_after: pydantic.AwareDatetime | None = None,
+    created_before: pydantic.AwareDatetime | None = None,
+    record_condition: list[schema.request.FieldCondition] | None = None,
+    start_key: int | None = None,
+):
     """Query records"""
     logger.info(f"Query records")
-    pass
+    response = service.record.RecordService("record_project_record_table").query_record(
+        table_id=table_id,
+        limit=limit,
+        category=category,
+        created_after=created_after,
+        created_before=created_before,
+        record_condition=record_condition,
+        start_key=start_key,
+    )
+    return response
 
 
-@router.get("/record/{table_id}/{record_id}", response_model=schema.table.Record)
-def get_record(table_id: str, record_id: str):
+@router.get("/record/{record_id}", response_model=schema.table.Record)
+def get_record(record_id: str):
     """Get a record"""
     logger.info(f"Get record: {record_id}")
-    pass
+    response = service.record.RecordService(
+        "record_project_record_table"
+    ).get_record_by_id(record_id)
+    return response
 
 
-@router.put("/record/{table_id}/{record_id}", response_model=schema.table.Record)
-def update_record(table_id: str, record_id: str, record: schema.table.Record):
+@router.put("/record/{record_id}", response_model=schema.table.Record)
+def update_record(record_id: str, record: schema.table.Record):
     """Update a record"""
     logger.info(f"Update record: {record_id}")
-    return record
-    pass
+    response = service.record.RecordService(
+        "record_project_record_table"
+    ).update_record(record)
+    return response
 
 
-@router.delete("/record/{table_id}/{record_id}")
-def delete_record(table_id: str, record_id: str):
+@router.delete("/record/{record_id}")
+def delete_record(record_id: str):
     """Delete a record"""
     logger.info(f"Delete record: {record_id}")
-    pass
+    response = service.record.RecordService(
+        "record_project_record_table"
+    ).delete_record(record_id)
+    return response
--- python_backend/src/service/__init__.py
+++ python_backend/src/service/__init__.py
@@ -1,3 +1,4 @@
 """service module"""
 
 from service import user
+from service import record
